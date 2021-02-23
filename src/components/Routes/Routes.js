2c2a541623ac27622d0475fab662fdf9f086975b
devagx <alungroome@hotmail.com>
2021-02-23 21:26:24 +0000

    REFACTOR: Linking pages to navbar

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -4,11 +4,12 @@ import WelcomePage from "../WelcomePage/WelcomePage";
 import Location from "../Location/Location";
 import Weather from "../Weather/Weather";
 import Reminders from "../Reminders/Reminders";
-import MyWalksPage from "../MyWalksPage/MyWalksPage";
+
 import AddWalksPage from "../AddWalksPage/AddWalksPage";
 import IndividualWalksPage from "../IndividualWalksPage/IndividualWalksPage";
 import Logout from "../Logout/Logout";
 */
+import MyWalksPage from "../MyWalksPage/MyWalksPage";
 import TestPage from "../TestPage/TestPage";
 
 function Routes() {
@@ -24,11 +25,12 @@ function Routes() {
                     <Route exact path="/Location" component={Location} />
                     <Route exact path="/Weather" component={Weather} />
                     <Route exact path="/Reminders" component={Reminders} />
-                    <Route exact path="/MyWalksPage" component={MyWalksPage} />
+                    
                     <Route exact path="/AddWalksPage" component={AddWalksPage} />
                     <Route exact path="/IndividualWalksPage" component={IndividualWalksPage} />
                     <Route exact path="/Logout" component={Logout} />
                     */}
+                    <Route exact path="/MyWalksPage" component={MyWalksPage} />
                     <Route exact path="/TestPage" component={TestPage} />
                 </Switch>
             </BrowserRouter>
