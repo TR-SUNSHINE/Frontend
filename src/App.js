1643007213a44147756ea6efcb1f8780bf4c0d13
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-23 10:16:02 +0000

    FEAT: adding react-bootstrap container component instead of div

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
 
 function App() {
   return (
-    <div className="App container">
+    <Container>
       {/* think navbar goes here so appears on all pages, i.e. above the router for individal pages */}
       <BrowserRouter>
         <Route exact path="/" component={WelcomePage} />
         {/* add other pages here */}
       </BrowserRouter>
-
-    </div>
+    </Container>
   );
 }
 
