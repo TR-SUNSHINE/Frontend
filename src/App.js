71d162897bd42c8095d64e18deb7cfa219737ebd
thurschm2 <thurschm2@gmail.com>
2021-02-23 15:16:42 +0000

    FEAT: regiterPage initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./index.css";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       {/* think navbar goes here so appears on all pages, i.e. above the router for individal pages */}
       <BrowserRouter>
         <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/register" component={RegisterPage} />
+
         {/* add other pages here */}
+
       </BrowserRouter>
 
     </div>
