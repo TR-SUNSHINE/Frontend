c565b0dcf1cc44243c17413976de7a884e506257
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-23 09:33:35 +0000

    FEAT: importing bootstrap css file and adding centering to heading in index.css for page titles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import "./App.css";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
 
 function App() {
   return (
     <div className="App container">
 
-        <BrowserRouter>
-          <Route exact path="/" component={WelcomePage}/>
-          {/* add other pages here */}
-        </BrowserRouter>
-     
+      <BrowserRouter>
+        <Route exact path="/" component={WelcomePage} />
+        {/* add other pages here */}
+      </BrowserRouter>
+
     </div>
   );
 }
