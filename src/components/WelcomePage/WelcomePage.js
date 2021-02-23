e94d7430eadb32f52a6cfe68f8498e30691352f1
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-23 12:40:56 +0000

    FEAT: using a div as a button container & text-align css property with media queries to align buttons at different sizes

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -12,21 +12,25 @@ const Welcome = () => {
         // <div class="row">
         <Row>
             <Col>
-                <h3 class="heading heading--main">Welcome to Sunshine</h3>
-                <div class="frame--sunshine">
-                    <img class="img--sunshine" src="./images/welcomeSunshine.jfif" alt=""></img>
+                <h3 className="heading heading--main">Welcome to Sunshine</h3>
+                <div className="frame--sunshine">
+                    <img className="img--sunshine" src="./images/welcomeSunshine.jfif" alt=""></img>
                 </div>
                 <Row>
                     {/* n.b. I'm still converting inside here to the react-bootstrap versions of columns: https://react-bootstrap.github.io/layout/grid/   */}
                     {/* <div class="col-sm-2"></div> */}
                     {/* <div class="col-sm-4 one"> */}
-                    <div class="col-sm-4 one">
-                        <Button>Register</Button>
-                    </div>
-                    <div class="col-sm-4 two">
-                        <Button>Login</Button>
-                    </div>
-                    {/* <div class="col-sm-2"></div> */}
+                    <Col xs={12} sm={12} md={6}>
+                        <div className="button__container button__container--left" >
+                            <Button>Register</Button>
+                        </div>
+                    </Col>
+
+                    <Col xs={12} sm={12} md={6}>
+                        <div className="button__container button__container--right" >
+                            <Button>Login</Button>
+                        </div>
+                    </Col>
                 </Row>
             </Col>
         </Row>
