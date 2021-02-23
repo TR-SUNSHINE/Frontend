64b4eb7962021a3a5134d759223e493a924e8f96
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-02-23 20:30:59 +0000

    FEAT_Walks_Component updtate main locally

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./index.css";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import WelcomePage from "./components/WelcomePage/WelcomePage";
+//import WelcomePage from "./components/WelcomePage/WelcomePage";
+import MyWalk from "./components/MyWalksPage/MyWalksPage";
 
 function App() {
   return (
     <Container>
       {/* think navbar goes here so appears on all pages, i.e. above the router for individal pages */}
       <BrowserRouter>
-        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/" component={MyWalk} />
         {/* add other pages here */}
       </BrowserRouter>
     </Container>
