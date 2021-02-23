4ac021052828dc232557b9c0a313ae1995a115e2
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-23 09:34:40 +0000

    FEAT: adding comment on App.js where I think navbar will go

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import WelcomePage from "./components/WelcomePage/WelcomePage";
 function App() {
   return (
     <div className="App container">
-
+      {/* think navbar goes here so appears on all pages, i.e. above the router for individal pages */}
       <BrowserRouter>
         <Route exact path="/" component={WelcomePage} />
         {/* add other pages here */}
