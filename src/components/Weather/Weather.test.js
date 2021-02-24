882bdce85ba8a0b545d6da1e539c3f9cf909f1e6
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-24 18:11:32 +0000

    FEAT: adding a test file & starting coding

---
+++ src/components/Weather/Weather.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Weather from "./Weather";
+
+describe("Weather component", () => {
+    afterEach(() => jest.clearAllMocks());
+
+    test(`Given the required props, 
+    When the component is rendered, then the weather description should be present.`, () => {
+
+
+
+    });
+});
+
