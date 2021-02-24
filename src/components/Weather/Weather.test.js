ea669127eadcbf58282f79998faeba7b9cd72b25
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-24 18:57:05 +0000

    FEAT: writing first tests

--- src/components/Weather/Weather.test.js
+++ src/components/Weather/Weather.test.js
@@ -1,14 +1,35 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Weather from "./Weather";
+import { faSun, faBolt, faWind, faSmog, faCloud, faRainbow, faPooStorm, faCloudSunRain, faCloudSun, faCloudShowersHeavy, faCloudRain, faSnowflake } from "@fortawesome/free-solid-svg-icons";
 
 describe("Weather component", () => {
     afterEach(() => jest.clearAllMocks());
 
+    const requiredWeatherTimes = [
+        { id: "001", time: "13:00", temperature: "22°", description: "sunny", icon: faSun },
+        { id: "002", time: "14:00", temperature: "23°", description: "lightening", icon: faBolt },
+        { id: "003", time: "15:00", temperature: "24°", description: "windy", icon: faWind },
+    ];
+
+    const requiredSelectedWeatherTime = { id: "002", time: "14:00", temperature: "23°", description: "lightening", icon: faBolt };
+
+    const requiredToggleWeatherTimeSelected = jest.fn();
+
     test(`Given the required props, 
-    When the component is rendered, then the weather description should be present.`, () => {
+    When the component is rendered, then an array of weather div elements should be present.`, () => {
 
+        render(<Weather weatherTimes={requiredWeatherTimes} selectedWeatherTime={requiredSelectedWeatherTime} toggleWeatherTimeSelected={requiredToggleWeatherTimeSelected} />);
 
+        expect(screen.getByText("windy")).toBeInTheDocument();
+        expect(screen.getByText("lightening")).toBeInTheDocument();
+        expect(screen.getByText("sunny")).toBeInTheDocument();
+        expect(screen.getByText("13:00")).toBeInTheDocument();
+        expect(screen.getByText("14:00")).toBeInTheDocument();
+        expect(screen.getByText("15:00")).toBeInTheDocument();
+        expect(screen.getByText("22°")).toBeInTheDocument();
+        expect(screen.getByText("23°")).toBeInTheDocument();
+        expect(screen.getByText("24°")).toBeInTheDocument();
 
     });
 });
