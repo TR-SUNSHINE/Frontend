ceb4ee0653daa6a8466a039a9263d5b310cce841
devagx <62251824+devagx@users.noreply.github.com>
2021-02-24 21:02:12 +0000

    FIX: resolved conflict

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import "./index.css";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Header from "./components/Header/Header";
-<<<<<<< HEAD
-=======
 import Routes from "./components/Routes/Routes";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
->>>>>>> main
 
 function App() {
   return (
