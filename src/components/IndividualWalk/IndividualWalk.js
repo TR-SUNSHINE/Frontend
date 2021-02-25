7da9dbd5dde435e208e2ee86178bb3dea9719de7
devagx <alungroome@hotmail.com>
2021-02-25 12:37:40 +0000

    REFACTOR: removed maps into its own component.

--- src/components/IndividualWalk/IndividualWalk.js
+++ src/components/IndividualWalk/IndividualWalk.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import RatingsBar from "../RatingsBar/RatingsBar";
 import Graph from "../Graph/Graph";
+import Map from "../Map/Map";
 import { useState } from "react";
 
 const IndividualWalk = ({ id }) => {
@@ -60,12 +61,7 @@ const IndividualWalk = ({ id }) => {
         <Row>
             <Col>
                 <h3 className="heading heading--main">Individual Walk</h3>
-                <div className="container d-flex justify-content-center align-items-center">
-                    <div className="card p-1">
-                        <iframe className="iframe--map"
-                            src="https://www.google.com/maps/d/embed?mid=1F0OhEou31qd5wCPlKahJ8INJa75su22D"></iframe>
-                    </div>
-                </div>
+                <Map />
                 <Row>
                     <Graph data={filteredUser} />
                 </Row>
---
+++ src/components/Map/Map.js
@@ -0,0 +1,15 @@
+import "./Map.css";
+
+const Map = ({ id }) => {
+
+    return (
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="card p-1">
+                <iframe className="iframe--map"
+                    src="https://www.google.com/maps/d/embed?mid=1F0OhEou31qd5wCPlKahJ8INJa75su22D"></iframe>
+            </div>
+        </div>
+    );
+};
+export default Map;
+
