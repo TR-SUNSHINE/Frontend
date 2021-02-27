5ee6803a45283286fb424885024b7cfb45c63bd4
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-27 14:48:34 +0000

    changing layout of buttons so Button & Link on separate lines for readability

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -19,13 +19,21 @@ const Welcome = () => {
                 <Row>
                     <Col xs={12} sm={12} md={6}>
                         <div className="button__container button__container--left" >
-                            <Button variant="accessible"><Link className="button--link" to="/register">Register</Link></Button>
+                            <Button variant="accessible">
+                                <Link className="button--link" to="/register">
+                                    Register
+                                </Link>
+                            </Button>
                         </div>
                     </Col>
 
                     <Col xs={12} sm={12} md={6}>
                         <div className="button__container button__container--right" >
-                            <Button variant="accessible"><Link className="button--link" to="/login">Login</Link></Button>
+                            <Button variant="accessible">
+                                <Link className="button--link" to="/login">
+                                    Login
+                                </Link>
+                            </Button>
                         </div>
                     </Col>
                 </Row>
