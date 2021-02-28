92fd6a7d745a223c10d80d035f3b168e2464c342
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-28 13:32:59 +0000

    FEAT: removing unecessary Columns as already in child component

--- src/components/WeatherContainer/WeatherContainer.js
+++ src/components/WeatherContainer/WeatherContainer.js
@@ -1,19 +1,23 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Weather from "../Weather/Weather";
 
 const WeatherContainer = ({ weatherTimes, selectedWeatherTime, toggleWeatherTimeSelected }) => {
+
     return (
-        <Row>
-            <Col>
-                <div className="weather__container">
+        <>
+            <Row>
+                <Col className="weather__container">
                     <h4 className="heading heading--secondary">Weather for the next 24 hours</h4>
                     <p> Click on a time then set your walk time reminder for today.</p>
-                    <Weather weatherTimes={weatherTimes} selectedWeatherTime={selectedWeatherTime} toggleWeatherTimeSelected={toggleWeatherTimeSelected} />
-                </div>
-            </Col>
-        </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Weather weatherTimes={weatherTimes} selectedWeatherTime={selectedWeatherTime} toggleWeatherTimeSelected={toggleWeatherTimeSelected} />
+            </Row>
+        </>
     );
 };
 
