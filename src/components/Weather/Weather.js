82f6bab017ae29c5722f6527076d11618d686d38
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-28 13:30:44 +0000

    FEAT: implementing scrollbar left & right buttons using useRef

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,45 +7,68 @@ import { showLocalTime } from "../../helperFunctions";
 import "./Weather.css";
 
 const Weather = ({ weatherTimes, selectedWeatherTime, toggleWeatherTimeSelected }) => {
+    const scrollRef = useRef();
+
+    const handleScroll = (direction) => {
+        if (direction === "left") {
+            scrollRef.current.scrollLeft -= 175;
+        } else {
+            scrollRef.current.scrollLeft += 175;
+        }
+    };
 
     return (
-        <Row>
+        <>
             <Col>
-                <div className="individual__weather__container">
-                    {weatherTimes.map((weather) => {
-
-                        return (
-                            <Button variant="weather-display" key={weather.dt} className={selectedWeatherTime === weather.dt ? "weather selected" : "weather"} onClick={(event) => { toggleWeatherTimeSelected(weather.dt); }}>
-                                <div>{showLocalTime(weather.dt)}</div>
-                                <Row>
-                                    <Col>temp:</Col>
-                                    <Col>{weather.temp}ºC</Col>
-                                </Row>
-                                <Row>
-                                    <Col>feels:</Col>
-                                    <Col>{weather.feels_like}ºC</Col>
-                                </Row>
-                                <Row>
-                                    <Col>feels:</Col>
-                                    <Col>{weather.feels_like}ºC</Col>
-                                </Row>
-                                <Row>
-                                    <Col>{weather.weather[0].description}</Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <Image src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
-                                    </Col>
-                                </Row>
-                                <p className="weather--description">{weather.description}</p>
-                            </Button>
-
-                        );
-                    })}
-                </div>
+                <Button onClick={() => handleScroll("left")
+                }>
+                    {"<"}
+                </Button>
             </Col>
-        </Row>
+            <Col
+                className="individual__weather__container"
+                ref={scrollRef}
+            >
+                {weatherTimes.map((weather) => {
+                    return (
+                        <Button
+                            variant="weather-display"
+                            key={weather.dt}
+                            className={selectedWeatherTime === weather.dt ? "weather selected" : "weather"}
+                            onClick={(event) => { toggleWeatherTimeSelected(weather.dt); }}
+                        >
+                            <div>{showLocalTime(weather.dt)}</div>
+                            <Row>
+                                <Col>temp:</Col>
+                                <Col>{weather.temp}ºC</Col>
+                            </Row>
+                            <Row>
+                                <Col>feels:</Col>
+                                <Col>{weather.feels_like}ºC</Col>
+                            </Row>
+                            <Row>
+                                <Col>{weather.weather[0].description}</Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Image src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                                </Col>
+                            </Row>
+                            <p className="weather--description">{weather.description}</p>
+                        </Button>
+                    );
+                })}
+            </Col>
+            <Col>
+                <Button onClick={() => handleScroll("right")}>
+                    >
+            </Button>
+            </Col>
+        </>
     );
+
 };
 
 export default Weather;
+
+
