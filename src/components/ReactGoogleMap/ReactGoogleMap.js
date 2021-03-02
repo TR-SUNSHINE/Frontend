cff0ad9600cadda127c2bd5535701066d7d841e0
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-02 13:02:22 +0000

    REACT: adding lastest attempt with React & google maps from article: not working

--- src/components/ReactGoogleMap/ReactGoogleMap.js
+++ src/components/ReactGoogleMap/ReactGoogleMap.js
@@ -1,44 +1,45 @@
-import React, { Component } from "react";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import axios from "axios";
 
+const ReactGoogleMap = () => {
 
-const mapStyles = {
-    width: "100%",
-    height: "100%"
-};
+    const ref = useRef();
+    const [map, setMap] = useState();
 
-const googleKey = process.env.REACT_APP_GOOGLE_API_KEY;
+    const googleKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
-class ReactGoogleMap extends Component {
+    useEffect(() => {
+        const onLoad = () => {
 
-    constructor() {
-        super();
-        this.state = {
-            name: "React"
+            console.log("in onLoad");
+
+            setMap(new window.google.maps.Map(ref.current,
+                {
+                    center: { lat: 53.46265, long: -2.24909 },
+                    zoom: 8
+                })
+            );
+
         };
-    }
 
-    render() {
-        return (
-            <div>
-                <Map
-                    google={this.props.google}
-                    zoom={14}
-                    style={mapStyles}
-                    initialCenter={{
-                        lat: 53.46265,
-                        lng: -2.24909
-                    }}
-                >
-                    <Marker
-                        onClick={this.onMarkerClick}
-                        name={"This is test name"}
-                    />
-                </Map>
-            </div>
-        );
-    }
+        if (!window.google) {
+
+            const script = document.createElement(`script`);
+            script.src = `https://maps.googleapis.com/maps/api/js?key=` + googleKey;
+            document.head.append(script);
+            script.addEventListener(`load`, onLoad);
+            return () => script.removeEventListener(`load`, onLoad);
+        };
+    });
+
+    console.log(map);
+    return (
+        <div
+            style={{ height: `30vh`, margin: `1em 0`, borderRadius: `0.5em` }}
+            {...ref}
+        />
+    );
 };
 
-export default GoogleApiWrapper({ apiKey: googleKey })(ReactGoogleMap);
+export default ReactGoogleMap;
 
