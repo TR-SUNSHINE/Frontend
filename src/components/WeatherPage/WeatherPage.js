333e0bd96b4624fce8ad5bffdcdf3dddd66a0739
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-10 12:14:19 +0000

    FEAT: updating WeatherPage to use GoogleMap component

--- src/components/WeatherPage/WeatherPage.js
+++ src/components/WeatherPage/WeatherPage.js
@@ -6,11 +6,14 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import WeatherContainer from "../WeatherContainer/WeatherContainer";
-import CurrLocationMapContainer from "../Map/CurrLocationMapContainer";
-import "./WeatherPage.css";
+import "../WeatherPage/WeatherPage.css";
 import "../Button/Button.css";
+import GoogleMap from "../Map/GoogleMap";
+import { GoogleApiWrapper, Marker } from "google-maps-react";
 
-const WeatherPage = () => {
+const googleKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+const WeatherPage = ({ google }) => {
 
     const [coords, setCoords] = useState({ lat: 0, long: 0 });
     const [weatherTimes, setWeatherTimes] = useState([]);
@@ -19,7 +22,6 @@ const WeatherPage = () => {
     const [selectedWeatherTime, setSelectedWeatherTime] = useState(0);
     const [reminder, setReminder] = useState({ time: 0, walk: "" });
     const weatherKey = process.env.REACT_APP_WEATHER_API_KEY;
-    const googleKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
     const toggleWeatherTimeSelected = (weatherId) => {
 
@@ -42,6 +44,7 @@ const WeatherPage = () => {
 
     const getCoords = useCallback(async () => {
 
+
         try {
 
             if (navigator && navigator.geolocation) {
@@ -51,25 +54,29 @@ const WeatherPage = () => {
                 });
 
                 const position = await location;
+
                 setCoords({ lat: position.coords.latitude, long: position.coords.longitude });
 
             } else {
 
-                // hard-coded Manchester coordinates
+                // Manchester coordinates
 
                 setCoords({ lat: 53.4809, long: -2.2374 });
+
             }
 
         } catch (error) {
             console.log(error);
             setNoResults(true);
         }
+
     }, []);
 
 
     const getWeather = useCallback(async () => {
 
         if (coords.lat && coords.long) {
+
             try {
 
                 const weatherApi = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&exclude=daily,minutely,alerts&units=metric&appid=${weatherKey}`);
@@ -90,26 +97,37 @@ const WeatherPage = () => {
                 console.log(error);
                 setNoResults(true);
             }
+
         }
 
     }, [coords.lat, coords.long, weatherKey]);
 
-
     useEffect(() => {
         getCoords();
         getWeather();
-    }, [getCoords, getWeather]);
+    }, [getWeather, getCoords]);
 
     if (noResults) {
         return <Redirect to="/NotFoundPage" />;
     }
 
+    console.log("in TestWeather", coords);
     return (
         <>
             <Row>
                 <Col>
                     <h3 className="heading heading--main">Weather today: {date}</h3>
-                    <CurrLocationMapContainer />
+                    <GoogleMap
+                        centerAroundCurrentLocation={false}
+                        lat={coords.lat}
+                        lng={coords.long}
+                        google={google}
+                        zoom={13}
+                        draggable={false}
+                        disableDoubleClickZoom={true}
+                    >
+                        <Marker lat={coords.lat} lng={coords.long} visible={true} />
+                    </GoogleMap>
                     <WeatherContainer
                         weatherTimes={weatherTimes}
                         selectedWeatherTime={selectedWeatherTime} toggleWeatherTimeSelected={toggleWeatherTimeSelected}
@@ -117,8 +135,6 @@ const WeatherPage = () => {
 
                 </Col>
             </Row >
-
-
 
             <Row>
                 <Col xs={12}>
@@ -138,4 +154,6 @@ const WeatherPage = () => {
     );
 };
 
-export default WeatherPage;
+export default GoogleApiWrapper({
+    apiKey: googleKey
+})(WeatherPage);
