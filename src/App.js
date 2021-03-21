92f0a495cf1d9666a86d8d7405cd0507e5ae10d1
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-21 14:18:24 +0000

    successfully calling getReminder endpoint

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Routes from "./components/Routes/Routes";
 function App() {
 
   const myUserId = "e9f9080b-4626-41db-8504-90896859f8e5";
+  const myReminderId = "94fc593a-7323-44ff-bb9c-13b833b38157";
 
   const getReminders = async () => {
 
@@ -18,8 +19,15 @@ function App() {
     console.log(reminders);
   };
 
+  const getReminder = async () => {
+
+    const reminders = await axios.get(`https://ia7thtfozg.execute-api.eu-west-2.amazonaws.com/users/${myUserId}/reminders/${myReminderId}`);
+
+    console.log(reminders);
+  };
+
   useEffect(() => {
-    getReminders();
+    getReminder();
     // return () => {
 
     // }
