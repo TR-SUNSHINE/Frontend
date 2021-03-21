00c56904e8e7dec0d489cd23e1aa9090fb6a18af
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-21 21:06:45 +0000

    moving getReminders to WeatherPage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, createContext } from "react";
+import { formatReminderTime, formatLocalDateTime } from "./helperFunctions";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./App.css";
@@ -9,35 +10,19 @@ import Routes from "./components/Routes/Routes";
 
 function App() {
 
-  const myUserId = "e9f9080b-4626-41db-8504-90896859f8e5";
-  const myReminderId = "9af357f1-67cc-4747-a21a-9a74113d7780";
+  //useEffect(() => {
+  ///getReminders();
+  // return () => {
 
-  const getReminders = async () => {
-
-    const reminders = await axios.get(`https://ia7thtfozg.execute-api.eu-west-2.amazonaws.com/users/${myUserId}/reminders`);
-
-    console.log(reminders);
-  };
-
-  const getReminder = async () => {
-
-    const reminders = await axios.get(`https://ia7thtfozg.execute-api.eu-west-2.amazonaws.com/users/${myUserId}/reminders/${myReminderId}`);
-
-    console.log(reminders);
-  };
-
-  useEffect(() => {
-    getReminders();
-    // return () => {
-
-    // }
-  }, []);
+  // }
+  //}, []);
 
   return (
     <>
       <Header />
       <Container>
         <Routes
+
         />
       </Container>
     </>
