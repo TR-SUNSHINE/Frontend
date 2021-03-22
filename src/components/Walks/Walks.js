2d96a505080ebd385f012ef8f268370bba781655
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-03-22 13:20:55 +0000

    add get api to walks component

--- src/components/Walks/Walks.js
+++ src/components/Walks/Walks.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../../index.css";
 import "./Walks.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 import "../Button/Button.css";
 
-const Walks = () => {
 
-    // const ratings = ["Manchester Museum", "Queens Park", "Trafford Centre"];
-    // const list = [];
 
-    // ratings.forEach((rating) => { list.push(<li>{rating}</li>); });
+const Walks = (props, userId) => {
+    userId = "3bd4d097-8193-11eb-b706-062d232c43b8";
+    const walkName = "";
+    const walkId = "";
+    const [walks, setWalks] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://gt63kubuik.execute-api.eu-west-2.amazonaws.com/Prod/v1/walks/user/${userId}`)
+            .then(response => setWalks(response.data))
+            .catch(error => console.log(error));
+    }, []);
+
     return (
         <Row>
             <Col>
-                <h5 className="review-stat" ><Link className="text--hyperlink" to="/IndividualWalk">Manchester Museum</Link></h5>
+                <h5 className="review-stat" ><Link className="text--hyperlink" to="/IndividualWalk">{walkName}</Link></h5>
             </Col>
         </Row>
     );
