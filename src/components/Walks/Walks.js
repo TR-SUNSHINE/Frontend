4a4d0e0893ec4301e101ebc06b719e55abaddf6c
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-03-23 17:41:46 +0000

    fetch walks from db using userid

--- src/components/Walks/Walks.js
+++ src/components/Walks/Walks.js
@@ -1,35 +1,26 @@
-import React, { useEffect } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React from "react";
 import "../../index.css";
 import "./Walks.css";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios";
 import "../Button/Button.css";
+import Walk from "../Walk/Walk";
 
 
-
-const Walks = (props, userId) => {
-    userId = "3bd4d097-8193-11eb-b706-062d232c43b8";
-    const walkName = "";
-    const walkId = "";
-    const [walks, setWalks] = useState([]);
-
-    useEffect(() => {
-        axios
-            .get(`https://gt63kubuik.execute-api.eu-west-2.amazonaws.com/Prod/v1/walks/user/${userId}`)
-            .then(response => setWalks(response.data))
-            .catch(error => console.log(error));
-    }, []);
-
+function Walks({ walks }) {
+    console.log(typeof walks);
     return (
-        <Row>
-            <Col>
-                <h5 className="review-stat" ><Link className="text--hyperlink" to="/IndividualWalk">{walkName}</Link></h5>
-            </Col>
-        </Row>
+        <div>
+            {
+                walks.map(walk =>
+
+                    <Walk
+                        walk={walk}
+
+                    />
+                )
+            }
+        </div>
+
     );
-};
+}
 
 export default Walks;
