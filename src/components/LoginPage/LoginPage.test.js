03aa99b520a7e3b1c0c742daec6db4640cda43f0
thurschm2 <thurschm2@gmail.com>
2021-03-24 21:12:56 +0000

    TEST: Login Page Back Button present

---
+++ src/components/LoginPage/LoginPage.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import LoginPage from "./LoginPage";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe("LoginPage component", () => {
+
+    test(`Given the component is rendered, 
+            then the Login to Sunshine text should be present`, () => {
+        render(
+            <Router>
+                <LoginPage />
+            </Router>
+        );
+        expect(screen.getByText("Login to Sunshine")).toBeInTheDocument();
+    });
+    test(`Given the component is rendered, 
+        then the Back button should be present`, () => {
+        render(
+            <Router>
+                <LoginPage />
+            </Router>
+        );
+        expect(screen.getAllByRole("button").find(button => button.textContent === "Back")).toBeInTheDocument();
+    });
+
+    test(`Given the required props,
+when the component is rendered, 
+then the Login button should be present`, () => { }
+    );
+
+    test(`Given the email input ,
+when the component is rendered, 
+then the Login button should be enabled`, () => { }
+    );
+    test(`Given the absence of email,
+when the component is rendered, 
+then the Login button should be disabled`, () => { }
+    );
+    test(`Given the email is rendered,
+when the login task is clicked, 
+then the submit function is called with the correct email`, () => { }
+    );
+});
