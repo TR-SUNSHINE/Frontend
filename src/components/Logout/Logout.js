5619a168efcbda4292211dc3eba2c6a34b8ee584
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-24 18:21:45 +0000

    STYLING: amending button classes so button displays in center of page in larger viewports

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -26,9 +26,9 @@ const Logout = () => {
                     </Col>
                 </Row>
                 <Row>
-                    <Col xs={12} sm={6} md={6}>
-                        <div className="button__container button__container--left" >
-                            <Button variant="double"><Link className="button--link" to="/Loginpage">Login</Link></Button>
+                    <Col xs={12}>
+                        <div className="button__container" >
+                            <Button variant="single"><Link className="button--link" to="/Loginpage">Login</Link></Button>
                         </div>
                     </Col>
                 </Row>
