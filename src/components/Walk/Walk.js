46a32e851ab046778e3bdb86e68e17dc0d555087
devagx <alungroome@hotmail.com>
2021-03-25 21:43:56 +0000

    FEATURE: Implemented delete walk.

--- src/components/Walk/Walk.js
+++ src/components/Walk/Walk.js
@@ -7,9 +7,20 @@ import "./Walk.css";
 import { Link } from "react-router-dom";
 import "../Button/Button.css";
 import RatingsBar from "../RatingsBar/RatingsBar";
-
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 function Walk({ walk }) {
+    const deleteWalk = () => {
+
+        axios.delete(`https://gt63kubuik.execute-api.eu-west-2.amazonaws.com/Prod/v1/walks/`, { data: { Id: walk.id } })
+            .then(response => {
+                window.location.reload(false);
+            })
+            .catch(
+                error => console.log(error)
+            );
+    };
     return (
         <Row>
             <Col xs={6}>
@@ -29,8 +40,10 @@ function Walk({ walk }) {
             <Col xs={6}>
                 <RatingsBar value={walk.aveRating} disabled={true} />
             </Col>
+            <Col xs={6}>
+                <Button onClick={deleteWalk}>Delete</Button>
+            </Col>
         </Row>
     );
 }
-
 export default Walk;
