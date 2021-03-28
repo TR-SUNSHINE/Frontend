959e6a6c826a4128731d55c3c337cd963365e40c
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-28 22:49:56 +0100

    TEST: basic test for AddWalk

--- src/components/AddWalk/AddWalk.test.js
+++ src/components/AddWalk/AddWalk.test.js
@@ -1,26 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { waitFor, render, screen, cleanup } from "@testing-library/react";
 import AddWalk from "./AddWalk";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("AddWalk component", () => {
-    afterEach(() => jest.clearAllMocks());
 
-    test(`Given the component is rendered, 
-            then the Add Walk text should be present`, () => {
-        jest.mock("axios", () => {
-            return {
-                interceptors: {
-                    request: { use: jest.fn(), eject: jest.fn() },
-                    response: { use: jest.fn(), eject: jest.fn() },
-                },
-            };
-        });
+    afterEach(cleanup);
+
+    test(`Given am AddWalk component is rendered, Before the component is rendered, 
+    The text "Loading... should be displayed`, async () => {
 
         render(
             <Router>
                 <AddWalk />
             </Router>
         );
-        expect(screen.getByText("Add a Walk")).toBeInTheDocument();
+
+        await waitFor(() => expect(screen.getByText("Loading...")).toBeInTheDocument());
+
     });
 });
