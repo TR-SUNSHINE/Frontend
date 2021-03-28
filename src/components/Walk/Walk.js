e7ec0a5dfa99f6395b212266b91a15dd51e670c4
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-28 21:44:04 +0100

    removing unused import

--- src/components/Walk/Walk.js
+++ src/components/Walk/Walk.js
@@ -1,7 +1,5 @@
 import "../../index.css";
 import React from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "../../index.css";
 import "./Walk.css";
 import { Link } from "react-router-dom";
