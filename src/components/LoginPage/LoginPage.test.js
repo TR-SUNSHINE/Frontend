77e8e568c76ac047da6b1c5d77081dd40c6596df
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-30 16:40:12 +0100

    adding in details props to test

--- src/components/LoginPage/LoginPage.test.js
+++ src/components/LoginPage/LoginPage.test.js
@@ -1,6 +1,35 @@
+
 import { fireEvent, render, screen } from "@testing-library/react";
 import LoginPage from "./LoginPage";
 import { BrowserRouter as Router } from "react-router-dom";
+
+
+const mockDetails = { selectedTime: 0, reminderId: "", reminderTime: 0, userId: "5e37e4d7-d053-4936-8827-01500c10a959", email: "", userName: "", emailError: "", loginError: "" };
+
+const setMockDetails = jest.fn();
+
+const mockProps = {
+
+    history: {
+        length: 16, action: "POP",
+        location: {
+            hash: "",
+            pathname: "/ErrorPage",
+            search: "",
+            state: ""
+        },
+        createHref: {},
+        push: {}
+    },
+    location: {
+        pathname: "/ErrorPage",
+        search: "",
+        hash: "",
+        state: { message: "Unable to load reminders" }
+    },
+    match: { path: "/ErrorPage", url: "/ErrorPage", isExact: true, params: {} },
+    setDetails: {},
+};
 
 describe("LoginPage component", () => {
 
@@ -8,7 +37,7 @@ describe("LoginPage component", () => {
             then the Login to Sunshine text should be present`, () => {
         render(
             <Router>
-                <LoginPage />
+                <LoginPage {...mockProps} details={mockDetails} setDetails={setMockDetails} />
             </Router>
         );
         expect(screen.getByText("Login to Sunshine")).toBeInTheDocument();
@@ -18,7 +47,7 @@ describe("LoginPage component", () => {
         then the Back button should be present`, () => {
         render(
             <Router>
-                <LoginPage />
+                <LoginPage {...mockProps} details={mockDetails} setDetails={setMockDetails} />
             </Router>
         );
         expect(screen.getAllByRole("button").find(button => button.textContent === "Back")).toBeInTheDocument();
@@ -29,7 +58,7 @@ describe("LoginPage component", () => {
         const { getByPlaceholderText, getByRole } =
             render(
                 <Router>
-                    <LoginPage />
+                    <LoginPage {...mockProps} details={mockDetails} setDetails={setMockDetails} />
                 </Router>
             );
         const input = getByPlaceholderText("Enter your email");
@@ -41,10 +70,12 @@ describe("LoginPage component", () => {
 
     test(`Login Button should not be disabled when email is not empty`, () => {
 
+        const mockDetailsWithEmail = { selectedTime: 0, reminderId: "", reminderTime: 0, userId: "5e37e4d7-d053-4936-8827-01500c10a959", email: "bob@bobsmith.co.uk", userName: "", emailError: "", loginError: "" };
+
         const { getByPlaceholderText, getByRole } =
             render(
                 <Router>
-                    <LoginPage />
+                    <LoginPage {...mockProps} details={mockDetailsWithEmail} setDetails={setMockDetails} />
                 </Router>
             );
         const input = getByPlaceholderText("Enter your email");
